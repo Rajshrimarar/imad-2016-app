769693f629b5d35a08ded557f80a1c22012d6328
Rajshrimarar <rajshree.marar@gmail.com>
2016-09-27 22:50:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res){
-    res.send('we are inside article one');
+    res.send('okay,we are inside article one');
     
 });
 app.get('/article-two', function(req,res){
