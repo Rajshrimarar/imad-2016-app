5877cac1b82371b696f36c014477ce1f8525d7e0
Rajshrimarar <rajshree.marar@gmail.com>
2016-10-11 14:39:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function(req,res){
+app.get('/article-onee', function(req,res){
     res.send('okay,we are inside article one');
     
 });
